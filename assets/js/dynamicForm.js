a69196d8ceec2edbc19be62b45d2bebcde9a0d54
jude <judeputhussery360@gmail.com>
2023-07-18 12:19:11 +0530

    animation to events section
    scroll animation
    added bg color to article tags

--- assets/js/dynamicForm.js
+++ assets/js/dynamicForm.js
@@ -1,5 +1,5 @@
 function getWorkDetails(isWork) {
-    var workDetails = document.querySelectorAll(".hideWork");
+    const workDetails = document.querySelectorAll(".hideWork");
     if (isWork.value == "Yes") {
         for (let i = 0; i < workDetails.length; i++) {
             workDetails[i].style.display = "block";
